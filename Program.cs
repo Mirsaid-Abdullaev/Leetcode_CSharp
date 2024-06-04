0f7d1b94cbe20376398b167866975321a48cefd5
Mirsaid Abdullaev <abdullaevm017@gmail.com>
2024-06-04 21:29:51 +0100

    Updated Program.cs to be included

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static Leetcode.EasyProblems;
+
+namespace Leetcode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsValid("{([{([])}])}"));
+            Console.ReadLine();
+        }
+    }
+}
